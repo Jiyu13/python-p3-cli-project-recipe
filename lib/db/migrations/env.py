9a4bd0e4998c1ac3ff814f9ff3080c3ebfae8ab1
Ziru Zou <jiyucoding@yahoo.com>
2023-03-07 20:53:58 -0500

    import sys

--- lib/db/migrations/env.py
+++ lib/db/migrations/env.py
@@ -18,6 +18,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+import sys
+[sys.path.append(i) for i in ['.', '..']]
+
 from models import Base
 target_metadata = Base.metadata
 
@@ -59,7 +62,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
